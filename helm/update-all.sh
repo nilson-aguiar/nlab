2d246f47f5fd35d663630263a8f9334e4178d973
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2024-11-07 12:07:49 +0100

    fix grafana deploy and remove unconfigured values

--- helm/update-all.sh
+++ helm/update-all.sh
@@ -2,7 +2,7 @@
 
 for i in ./*/overlay; do cd "$i" || continue ; kubectl apply -k .; cd - || exit; done
 
-for i in ./*/helmfile.yaml; do helmfile -f "$i" apply; done
+for i in ./*/helmfile.yaml; do helmfile -q -f "$i" apply; done
 
 
 
