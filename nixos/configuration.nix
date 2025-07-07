133e75d9698de1191341d94ae524aee8ab16578c
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2025-07-07 21:47:42 +0200

    update nix config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -156,6 +156,12 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
+  nix.settings.download-buffer-size = 524288000;
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "daily";
+  nix.gc.options = "--delete-older-than 7d"; 
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
