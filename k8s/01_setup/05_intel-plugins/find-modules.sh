2e7c410d3ad1cb29b6f1abf901241e6e72c25d7f
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2025-11-23 11:35:45 +0100

    trying to fix cloudflare tunnels

--- k8s/01_setup/05_intel-plugins/find-modules.sh
+++ k8s/01_setup/05_intel-plugins/find-modules.sh
@@ -2,7 +2,7 @@
 set -e
 
 KERNEL_VERSION=$(uname -r)
-MODULES_BUILTIN=$(find /host-lib/modules/${KERNEL_VERSION} -name modules.builtin)
+MODULES_BUILTIN=$(find /nix/store -maxdepth 1 -type d -regextype posix-extended -regex "/nix/store/[a-z0-9]{32}-linux-$(uname -r)$")
 
 if [ -n "${MODULES_BUILTIN}" ]; then
   echo "--kconfig-file=${MODULES_BUILTIN}"
