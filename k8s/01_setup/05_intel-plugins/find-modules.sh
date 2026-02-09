45dc771dc4b9375edf4ad681849623ca781438e0
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2026-02-09 01:36:59 +0100

    remove nfd

--- k8s/01_setup/05_intel-plugins/find-modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-KERNEL_VERSION=$(uname -r)
-MODULES_BUILTIN=$(find /nix/store -maxdepth 1 -type d -regextype posix-extended -regex "/nix/store/[a-z0-9]{32}-linux-$(uname -r)$")
-
-if [ -n "${MODULES_BUILTIN}" ]; then
-  echo "--kconfig-file=${MODULES_BUILTIN}"
-fi
